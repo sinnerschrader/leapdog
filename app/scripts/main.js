5bb322b54673844c437c28e9a93d27f9eb74c2a1
Rafael Epplee <raffomania@gmail.com>
2014-09-19 01:57:43 +0200

    Added rudimentary wheel

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,20 @@
-var $gestures = $(".gestures"),
-    $pos = $(".position");
+"use strict";
 
-var controller = new Leap.Controller({enableGestures: true})
+var $gestures = $(".gestures"),
+    $pos = $(".position"),
+    $wheel = $(".wheel");
+
+new Leap.Controller({enableGestures: true})
 .connect()
-.on('frame', function(frame){
+.on("frame", function(frame){
     if (frame.hands[0]) {
-        $pos.text(frame.hands[0].palmPosition[0]);
+        var x = frame.hands[0].palmPosition[0];
+        $pos.text(x);
+        $wheel.css({"-webkit-transform":"rotate("+x/2+"deg)"});
     }
+
     $gestures.text("");
     $.each(frame.gestures, function(i, gesture) {
-        $gestures.append(gesture.type);
+        $gestures.append(gesture.type + " ");
     });
-})
+});
